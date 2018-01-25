5af489833a03e0a3d4aeff4da892bac943030193
Ala Al-Fuqaha <ala@alas-mbp.attlocal.net>
2018-01-25 03:00:08 -0500

    commit

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -10,6 +10,9 @@ const add = require('../controllers/add.server.controller');
 // Load the 'sum' controller
 const sum = require('../controllers/sum.server.controller');
 
+// Load the 'partials' controller
+const partials = require('../controllers/partials.server.controller');
+
 // Define the routes module' method
 module.exports = function(app) {
 	// Mount the 'index' controller's 'render' method
@@ -18,4 +21,5 @@ module.exports = function(app) {
 	app.get('/add', add.render);
 	app.get('/sum', sum.render);
 	app.post('/add', add.render);
+	app.get('/partials', partials.render);
 };
